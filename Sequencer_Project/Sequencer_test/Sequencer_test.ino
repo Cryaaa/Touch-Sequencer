8fb5c00a192062b39f4e44ff35c25e3c830e756e
Cryaaa <ryan.savill@web.de>
2020-05-14 16:17:15 +0200

    working version (less freezing than interrupt)

--- Sequencer_Project/Sequencer_test/Sequencer_test.ino
+++ Sequencer_Project/Sequencer_test/Sequencer_test.ino
@@ -7,8 +7,12 @@ TTP229 ttp229;
 // position represents the current position in the sequence
 volatile int position = 0;
 
-// interrupt_pin 0 is digital pin 2 on the arduino uno
-const int interrupt_pin = 0;
+// clock pin configured to pin 2 (digital)
+const int clock_pin = 2;
+
+// define clock pin input states
+int clock_state = LOW;
+int clock_state_old = LOW;
 
 // pad_active is false if no pad has been touched since the last interrupt
 volatile bool pad_active = false;
@@ -23,56 +27,13 @@ int direction_pin_value_old = LOW;
 // define direction variable
 volatile int direction_variable = 1;
 
+// function to increase position without going into negative values
 void increase_position(){
     position = (position + direction_variable)%8;
     if (position < 0) {
         position = position + 8;
     }
 }
-
-// get's called every clock tick and advances the sequencer based on touchpad state
-void clock_trigger(){
-    // lets the sequencer advance one step if no pad is activated
-    if (!pad_active) {
-        for (int x = 0; x < 8; x++) {
-            if (x == position) {
-                digitalWrite(x+3, HIGH);
-            }
-            else {
-                digitalWrite(x+3, LOW);
-            }
-        }
-        increase_position();
-    }
-    
-    // otherwise some pads have been pressed and the sequencer finds the next step
-    // mod 8 from current position to proceed 
-    else {
-        // here position get's set to the next active pad value
-        while (!(pad_state[position])) {
-            Serial.println(pad_state[position]);
-            increase_position();
-            }
-        
-
-        // advances the sequence to the just determined step
-        for (int x = 0; x < 8; x++) {
-            if (x == position) {
-                digitalWrite(x+3, HIGH);
-            }
-            else {
-                digitalWrite(x+3, LOW);
-            }
-        }
-
-        // cleanup
-        increase_position();
-        pad_active = false;
-        for (int i = 0; i<8; i++){
-            pad_state[i] = false;
-        }
-    }
-} 
 
 
 void setup()
@@ -85,10 +46,10 @@ void setup()
         pinMode(i,OUTPUT);
     }
 
-    // definition interrupt pin
-    pinMode(interrupt_pin, INPUT);
-    attachInterrupt(interrupt_pin, clock_trigger, RISING);
+    // definition clock pin to digital pin
+    pinMode(clock_pin, INPUT);
 
+    // define direction pin 
     pinMode(direction_pin, INPUT);
 
     
@@ -109,7 +70,6 @@ void loop()
 
     // read direction pin state and write into current value
     direction_pin_value = digitalRead(direction_pin);
-
     // see if rising edge is present and change direction
     if (direction_pin_value == HIGH && direction_pin_value_old == LOW) {
         direction_variable = direction_variable * -1;
@@ -117,5 +77,52 @@ void loop()
     // set old direction to the current
     direction_pin_value_old = direction_pin_value;
 
+
+    // clock state setting
+    clock_state = digitalRead(clock_pin);
+    // loop for incoming rising edge
+    if (clock_state == HIGH && clock_state_old == LOW) {
+      
+        // lets the sequencer advance one step if no pad is activated
+        if (!pad_active) {
+            for (int x = 0; x < 8; x++) {
+                if (x == position) {
+                    digitalWrite(x+3, HIGH);
+                }
+                else {
+                    digitalWrite(x+3, LOW);
+                }
+            }
+            increase_position();
+        }
     
+        // otherwise some pads have been pressed and the sequencer finds the next step
+        // mod 8 from current position to proceed 
+        else {
+            // here position get's set to the next active pad value
+            while (!(pad_state[position])) {
+                Serial.println(pad_state[position]);
+                increase_position();
+            }
+
+            // advances the sequence to the just determined step
+            for (int x = 0; x < 8; x++) {
+                if (x == position) {
+                    digitalWrite(x+3, HIGH);
+                }
+                else {
+                    digitalWrite(x+3, LOW);
+                }
+            }
+
+            // cleanup
+            increase_position();
+            pad_active = false;
+            for (int i = 0; i<8; i++){
+                pad_state[i] = false;
+            }
+        }
+    }
+    // capture history of clock state
+    clock_state_old = clock_state;    
 }
